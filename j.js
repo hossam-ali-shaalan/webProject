4a9df59e9ae995123794ac06310f3d2d0e0d65fd
hossam ali <65079183+hossam-ali-shaalan@users.noreply.github.com>
2021-06-11 16:27:24 +0200

    Update j.js

--- j.js
+++ j.js
@@ -3,7 +3,5 @@ let landing=document.querySelector(".nava");
 imgsArray=["01.jpg","02.jpg","03.jpg","04.jpg"];
 setInterval(()=>{
     x=Math.floor(Math.random()*4);
-landing.style.backgroundImage='url("imgs/'+imgsArray[x]+'")';
+landing.style.backgroundImage='url('+imgsArray[x]+')';
 },4000)
-
-
